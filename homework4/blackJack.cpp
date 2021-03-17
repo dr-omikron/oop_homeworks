7f1c24109b5a1fd5f4538d01f79a9c3bc346a2fe
dr-omikron <andrey163.v@yandex.ru>
2021-03-17 20:10:05 +0400

    add dir homework4 and files blackJack.cpp, homework_04

---
+++ homework4/blackJack.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <vector>
+
+class Card
+{
+public:
+    enum Suit
+    {
+        Hearts,
+        Spades,
+        Clubs,
+        Diamonds
+
+    };
+    enum Rank
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+
+    };
+    Card(Rank r, Suit s, bool fU);
+    void Flip() { m_faceUp = !(m_faceUp); }
+    int GetValue() const;
+private:
+    Suit m_suit;
+    Rank m_rank;
+    bool m_faceUp;
+};
+
+class Hand 
+{
+protected:
+    std::vector<Card*> m_cards;
+public:
+    Hand() { m_cards.reserve(7); }
+    void Add (Card *newCard);
+    int GetTotal() const;
+    void Clear();
+    virtual ~Hand() { Clear(); }
+};
+
+Card::Card(Rank r, Suit s, bool fU)
+    :m_rank(r), m_suit(s), m_faceUp(fU){}
+
+int Card::GetValue() const
+{
+    int value = 0;
+    if (m_faceUp)
+    {
+        value = m_rank;
+        if (value > 10) { value = 10; }
+    }
+    return value;
+}
+
+void Hand::Add(Card *newCard)
+{
+    m_cards.push_back(newCard);
+}
+int Hand::GetTotal() const
+{
+    if (m_cards.empty()) {return 0;}
+    if (m_cards[0]->GetValue() == 0) {return 0;}
+    int total = 0;
+    std::vector<Card *>::const_iterator it;
+    for (it = m_cards.begin(); it != m_cards.end(); ++it)
+    {
+        total += (*it)->GetValue();
+    }
+    bool containsAce = false;
+    for (it = m_cards.begin(); it != m_cards.end(); ++it)
+    {
+        if ((*it)->GetValue() == Card::Ace)
+        {
+            containsAce = true;
+        }
+    }
+    if (containsAce && total <= 11) {total += 10;}
+    return total;
+}
+void Hand::Clear()
+{
+    std::vector<Card *>::iterator it = m_cards.begin();
+    for (it = m_cards.begin(); it != m_cards.end(); ++it)
+    {
+        delete *it;
+        *it = 0;
+    }
+    m_cards.clear();
+}
+
+int main()
+{
+    
+}
+
---
+++ homework4/homework_04.cpp
@@ -0,0 +1,192 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <utility> 
+
+//Ex 1
+
+class ArrayInt
+{
+private:
+	int m_length;
+	int *m_data;
+public:
+	ArrayInt() : m_length(0), m_data(nullptr) {}
+	ArrayInt(int length);
+	~ArrayInt() { delete[] m_data; }
+	int getLength() { return m_length; }
+	void erase();
+	int &operator[](int index);
+	void resize(int newLength);
+	void insertBefore(int value, int index);
+	void push_back(int value) { insertBefore(value, m_length); }
+	void pop_back();
+	void pop_front();
+	void sort();
+	void print();
+	void fill();
+};
+
+ArrayInt::ArrayInt(int length) : m_length(length)
+{
+	assert(length >= 0);
+	if (length > 0)
+		m_data = new int[length];
+	else
+		m_data = nullptr;
+}
+void ArrayInt::erase()
+{
+	delete[] m_data;
+	m_data = nullptr;
+	m_length = 0;
+}
+int &ArrayInt::operator[](int index)
+{
+	assert(index >= 0 && index < m_length);
+	return m_data[index];
+}
+void ArrayInt::resize(int newLength)
+{
+	if (newLength == m_length)
+		return;
+	if (newLength <= 0)
+	{
+		erase();
+		return;
+	}
+	int *data = new int[newLength];
+	if (m_length > 0)
+	{
+		int elementsToCopy = (newLength > m_length) ? m_length : newLength;
+		for (int index = 0; index < elementsToCopy; ++index)
+			data[index] = m_data[index];
+	}
+	delete[] m_data;
+	m_data = data;
+	m_length = newLength;
+}
+void ArrayInt::insertBefore(int value, int index)
+{
+	assert(index >= 0 && index <= m_length);
+	int *data = new int[m_length + 1];
+	for (int before = 0; before < index; ++before)
+		data[before] = m_data[before];
+	data[index] = value;
+	for (int after = index; after < m_length; ++after)
+		data[after + 1] = m_data[after];
+	delete[] m_data;
+	m_data = data;
+	++m_length;
+}
+
+void ArrayInt::pop_back()
+{
+	
+	int *data = new int[m_length - 1];
+	for (int i = 0; i < m_length - 1; ++i)
+		data[i] = m_data[i];
+	delete[] m_data;
+	m_data = data;
+	--m_length;
+}
+
+void ArrayInt::pop_front()
+{
+	int *data = new int[m_length - 1];
+	for (int i = 0; i < m_length - 1; ++i)
+		data[i] = m_data[i+1];
+	delete[] m_data;
+	m_data = data;
+	--m_length;
+}
+
+void ArrayInt::sort()
+{
+	for (int i = 0; i < m_length - 1; ++i)
+	{
+		int smIndx = i;
+		for (int j = i+1; j < m_length; j++)
+		{
+			if (m_data[j] < m_data[smIndx])
+				smIndx = j;
+		}
+		std::swap(m_data[i], m_data[smIndx]);
+	}
+}
+
+void ArrayInt::fill()
+{
+	for (int i = 0; i < m_length; ++i)
+		m_data[i] = rand() % 100;
+}
+
+void ArrayInt::print()
+{
+	for (int i = 0; i < m_length; ++i)
+		std::cout << m_data[i] << " ";
+	std::cout << "" << std::endl;
+}
+
+
+//Ex 2
+
+int DiffNumCounter(std::vector <int> &vec)
+{
+	int val = 0;
+	int counter = 0;
+	std::sort(vec.begin(), vec.end());
+	std::vector<int> :: iterator it;
+	std::vector<int> :: iterator it2;
+	it = vec.begin();
+	it2 = vec.begin();
+	it2++;
+	while (it != vec.end())
+	{
+		if (val != *it)
+		{
+			while (it2 != vec.end())
+			{
+				if (*it2 = !*it) { counter++;	break; }
+				else { break; }
+			
+			}
+		}   
+	val = *it;
+	it++;
+	if(it2 != vec.end())
+	it2++;
+	}
+	return counter;
+}
+
+void PrintCounter(std::vector <int> &vec)
+{
+	std::cout << "Different numbers in vector: "
+		<< DiffNumCounter(vec) << std::endl;
+}
+
+int main()
+{
+	//Ex 1
+	ArrayInt arr(10);
+	arr.erase();
+	arr.push_back(235);
+	arr.print();
+	arr.resize(10);
+	arr.fill();
+	arr.insertBefore(569,3);
+	arr.push_back(235);
+	arr.print();
+	arr.sort();
+	arr.print();
+	arr.pop_back();
+	arr.pop_front();
+	arr.print();
+	
+	//Ex 2
+	std::vector <int> vec = { 2, 4, 5, 5, 5, 9, 4, 4, 8, 3, 10 };
+	PrintCounter(vec);
+}
+
